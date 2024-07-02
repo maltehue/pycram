247586dda75262ee39620fddef4dbbb6ea171e7f
malte <huerkamp@uni-bremen.de>
2024-07-02 16:11:43 +0200

    automatically start giskard in binder

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,8 +3,7 @@
 source ${PYCRAM_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=${PYCRAM_WS}/src/pycram/binder/rviz_configs/pr2_config.json &
-cp ${PYCRAM_WS}/src/pycram/binder/webapps.json ${PYCRAM_WS}/src/rvizweb/webapps/app.json 
-
-roslaunch giskardpy giskardpy_stretch_standalone.launch &
+roslaunch -- wait giskardpy giskardpy_stretch_standalone.launch &
+cp ${PYCRAM_WS}/src/pycram/binder/webapps.json ${PYCRAM_WS}/src/rvizweb/webapps/app.json
 
 xvfb-run exec "$@"
